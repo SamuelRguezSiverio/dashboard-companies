e46d40651e0ff736611a4c38b162e4cce02bb0f2
SamuelRguezSiverio <samuelsrs89@gmail.com>
2024-05-20 12:17:08 +0100

    Total Suscribers Graphic

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -3,6 +3,7 @@ import CardSummary from './components/CardSummary/CardSummary'
 import { BookOpenCheck, UsersRound, Waypoints } from 'lucide-react'
 import LastCustomers from '@/components/LastCustomers/LastCustomers'
 import SalesDistributors from '@/components/SalesDistributors/SalesDistributors'
+import TotalSuscribers from '@/components/TotalSuscribers/TotalSuscribers'
 
 export const dataCardsSummary = [
   {
@@ -46,8 +47,12 @@ export default function Home() {
         )}
       </div>
       <div className="grid grid-cols-1 mt-12 xl:grid-cols-2 md:gap-x-10">
-        <LastCustomers/>
-        <SalesDistributors/>
+        <LastCustomers />
+        <SalesDistributors />
+      </div>
+      <div className="flex-col xl:flex xl:flex-row gap-y-4 md:gap-y-0 mt-12 md:mb-10 justify-center">
+        <TotalSuscribers/>
+        <p>List integration</p>
       </div>
     </div>
   )

cdb28aabd8f6ba422eef7a0e25c3ab2bd30b2b41
SamuelRguezSiverio <samuelsrs89@gmail.com>
2024-05-22 10:32:15 +0100

    Companies Form Creation

---
+++ app/(routes)/companies/page.tsx
@@ -0,0 +1,10 @@
+import { HeaderCompanies } from "./components/HeaderCompanies";
+
+export default function page() {
+  return (
+    <div>
+      <HeaderCompanies/>
+      <p>List Companies</p>
+    </div>
+  )
+}
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
   ignoredRoutes: ["/favicon.ico", "/sign-in", "/not-found"],
-  publicRoutes: ["/", '/logo.svg'],
+  publicRoutes: ["/", '/logo.svg', "/api/uploadthing"],
 
 });
 
